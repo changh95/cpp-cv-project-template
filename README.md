a33956c0bf874213a40af47f34bfba9bea9ba29a
changh95 <hyunggi.chang95@gmail.com>
2021-03-13 23:01:42 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# cpp-cv-project-template
+
+## Scripts
+
+[Wiki for install_llvm_toolchain.sh](https://github.com/nachovizzo/must-have-tools/wiki)
---
+++ run.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+import subprocess
+
+
+def main():
+    parser = argparse.ArgumentParser(description='42dot implementation tasks')
+    parser.add_argument('-d', action='store_true',
+                        help='Flag for debug mode compilation')
+    args = parser.parse_args()
+
+    os.system('rm ./bin/solution')
+    os.system('rm -rf ./CMakeFiles')
+    os.system('rm ./cmake_install.cmake')
+    os.system('rm ./Makefile')
+
+    print("/--------------------------------/")
+    print("        Configuring CMake...")
+    print("/--------------------------------/")
+    if args.d:
+        os.system('cmake . -DCMAKE_BUILD_TYPE=Debug')
+    else:
+        os.system('cmake . -DCMAKE_BUILD_TYPE=Release')
+
+    print("")
+    print("/--------------------------------/")
+    print("        Building target...")
+    print("/--------------------------------/")
+    os.system('make')
+
+    print("")
+    print("/--------------------------------/")
+    print("       Run solution program")
+    print("/--------------------------------/")
+    print("")
+    os.system('./bin/solution')
+
+
+if __name__ == '__main__':
+    main()
