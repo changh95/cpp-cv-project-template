2b9d37285ed2be61d2b14842c6060c588d92d47d
pacientes <pacientes.jw@gmail.com>
2021-04-23 01:16:47 +0900

    support opencv cmakelists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#ifdef OPENCV_DEVELOP
+#include <opencv4/opencv2/core.hpp>
+#endif
+
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
+
+	#ifdef OPENCV_DEVELOP
+	std::cout << "OpenCV Version: " << cv::getVersionString() << std::endl;
+	#endif
+
+	return 0;
 }
