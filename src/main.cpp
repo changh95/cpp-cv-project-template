3f3a02cd1f44747c4c99d915193764a12b49efa7
pacientes <pacientes.jw@gmail.com>
2021-04-25 00:21:17 +0900

    support PCL(Point Cloud Libary)

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,21 @@
 #include <opencv2/core.hpp>
 #endif
 
+#ifdef PCL_DEVELOP
+#include <pcl/pcl_base.h>
+#include <pcl/pcl_config.h>
+#endif
+
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
 
 	#ifdef OPENCV_DEVELOP
 	std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+	#endif
+
+	#ifdef PCL_DEVELOP
+	std::cout << "PCL(Point Cloud Libarry) Version: " << PCL_VERSION_PRETTY << std::endl;
 	#endif
 
 	return 0;
